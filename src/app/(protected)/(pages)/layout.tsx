07670682dba9e69f281eae6881850694b2682be6
Leonardo Carrijo <lh83712@gmail.com>
2025-07-13 19:41:33 -0300

    ✨ feat: Criação da infobar
    Criação da infobar que fica no topo da página. Inicialmente apenas com o botão para recolher a navbar e o campo de pesquisa, mas ainda serão adicionados outros elementos

--- src/app/(protected)/(pages)/layout.tsx
+++ src/app/(protected)/(pages)/layout.tsx
@@ -1,7 +1,8 @@
 import { getRecentProjects } from '@/actions/project'
 import { onAuthenticateUser } from '@/actions/user'
 import AppSidebar from '@/components/global/app-sidebar'
-import { SidebarProvider } from '@/components/ui/sidebar'
+import UpperInfoBar from '@/components/global/upper-info-bar'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
@@ -22,6 +23,11 @@ async function Layout({ children }: Props) {
                 user={checkUser.user}
                 recentProjects={recentProjects.data || []}
             />
+            <SidebarInset>
+                <UpperInfoBar user={checkUser.user}>
+                    {children}
+                </UpperInfoBar>
+            </SidebarInset>
         </SidebarProvider>
     )
 }
