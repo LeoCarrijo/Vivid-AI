9fd5ccdbfaf1d7208aebeeea24d932b4c9c017b0
Leonardo Carrijo <lh83712@gmail.com>
2025-07-13 18:59:30 -0300

    🚚 chore: Renomeando as pastas
    renomeando a pasta "src\app\(protected)\(pages)\(dashboard)" para "src\app\(protected)\(pages)\(dashboardPages)"

--- src/app/(protected)/(pages)/(dashboard)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import {getAllProjects} from "@/actions/project";
-
-const DashboardPage = async () => {
-    const projects = await getAllProjects()
-
-    return <div>DashboardPage</div>
-}
-
-export default DashboardPage
