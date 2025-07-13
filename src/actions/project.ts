cb518817cfeb47d43a6441480d7d6dc3aa3a054c
Leonardo Carrijo <lh83712@gmail.com>
2025-07-13 18:58:37 -0300

    ✨ feat: Método para buscar projetos
    Criação de buscar os métodos dos usuários

--- src/actions/project.ts
+++ src/actions/project.ts
@@ -2,6 +2,7 @@
 
 import {onAuthenticateUser} from "@/actions/user";
 import {client} from "@/lib/prisma";
+import { redirect } from "next/navigation";
 
 export async function getAllProjects() {
     try {
@@ -22,7 +23,36 @@ export async function getAllProjects() {
         })
 
         if (projects.length === 0) {
-            return {status: 404, error: "No projects found or this user"};
+            return {status: 404, error: "No projects found"};
+        }
+
+        return {status: 200, data: projects}
+    } catch (error) {
+        console.log('🔴 ERROR', error)
+        return {status: 500, error: "Internal server error"}
+    }
+}
+
+export async function getRecentProjects() {
+    try {
+        const checkUser = await onAuthenticateUser()
+        if(checkUser.status !== 200 || !checkUser.user) {
+            return {status: 403, error: "User not authenticated"}
+        }
+
+        const projects = await client.project.findMany({
+            where: {
+                userId: checkUser.user.id,
+                isDeleted: false
+            },
+            orderBy: {
+                updatedAt: 'desc'
+            },
+            take: 5
+        })
+
+        if(projects.length === 0) {
+            return{status: 404, error: 'No recent projects available'}
         }
 
         return {status: 200, data: projects}
